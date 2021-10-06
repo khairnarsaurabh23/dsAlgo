628dd002625345d5f8700634e9efbba889c6b3f2
Khairnar Saurabh Ravindra <srkhairnar23@gmail.com>
2021-10-06 22:25:05 +0530

    C++ program to find first and last element of the array

---
+++ s&s/firstLast.cpp
@@ -0,0 +1,18 @@
+//C++ program to find first and last emenent in the array
+//khairnar saurabh
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int  num;
+
+    int array[] = {2, 4, 23, 56, 12, 34, 56};
+     
+    //number of elements in an array
+    num = sizeof(array)/sizeof(array[0]);
+
+    cout<<"First Element: "<<array[0]<<endl;
+    cout<<"Last element: "<<array[n]<<endl;
+}
