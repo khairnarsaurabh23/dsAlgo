dc71607b2082e4cda6b7c660e51fde4762f0a3aa
Khairnar Saurabh Ravindra <srkhairnar23@gmail.com>
2021-10-11 20:28:37 +0530

    detect the loops in a linked list

---
+++ linkedList/detectLoop.cpp
@@ -0,0 +1,59 @@
+//C++ program to detect the loop in linked list
+//khairnar saurabh
+
+#include <bits/stdc++.h>
+using namespace std;
+///////////////////////////////////////////////////////////////////////////////
+
+struct Node                                                             //Node to hold linked list
+{
+    int data;
+    Node* next;
+};
+/////////////////////////////////////////////////////////////////////////////////
+
+void addNode(Node** head, int data)                                     //function to add a node
+{
+    Node* newNode = new Node;
+    newNode->data = data;
+    newNode->next = *head;
+    *head = newNode; 
+}
+
+bool detectLoop(Node *head)                                             //function to detect a loop
+{                                                                       //return true if loop is found, else false
+    unordered_set<Node*> s;                                             //hold pointers to nodes in unordered way
+    while (head != NULL)
+    {
+        if (s.find(head) != s.end())
+            return true;
+        
+        s.insert(head);
+        head = head->next;
+    }
+    return false;
+}
+//////////////////////////////////////////////////////////////////////
+
+int main()
+{
+    Node* head = NULL;
+
+    addNode(&head, 20);                                                 //adding nodes 
+    addNode(&head, 2);
+    addNode(&head, 23);
+    addNode(&head, 0);
+    addNode(&head, 56);
+    addNode(&head, 54);
+    addNode(&head, 1);
+    addNode(&head, 10);
+
+    head->next->next->next->next->next->next->next->next = head;          //create a loop
+
+    if (detectLoop(head))
+        cout<<"Loop Found";
+    else
+        cout<<"No Loop";
+
+    return 0;
+}
